d13867b5c13c6a8cdda53f52141775dc94ac0ffd
mahmoud <mahmoud.abuattiy106@gmail.com>
2022-03-03 14:48:19 +0200

    fix concept

--- conceptTyped.js
+++ conceptTyped.js
@@ -1,18 +1,18 @@
-  ////////////////////////////////////////////
-  var typed = new Typed('#concept', {
-    strings: [ "We are a classic American steakhouse with a relaxed and inviting atmosphere, and we stand for quality in all that we do. Our service and our food is guaranteed! We only use the finest cuts of meat. All our meat is aged for at least 28 days, and we guarantee that you will never receive anything less than Prime."],
-		// typing speed
-		typeSpeed: 20,
-		// time before typing starts
-		startDelay: 500,
-		// backspacing speed
-		backSpeed: 60,
-		// time before backspacing
-		backDelay: 2000,
-		// loop
-		loop: false,
-		// false = infinite
-		loopCount: Infinity,
-		// show cursor
-		showCursor: false
-  });
+  // ////////////////////////////////////////////
+  // var typed = new Typed('#concept', {
+  //   strings: [ ""],
+	// 	// typing speed
+	// 	typeSpeed: 20,
+	// 	// time before typing starts
+	// 	startDelay: 500,
+	// 	// backspacing speed
+	// 	backSpeed: 60,
+	// 	// time before backspacing
+	// 	backDelay: 2000,
+	// 	// loop
+	// 	loop: false,
+	// 	// false = infinite
+	// 	loopCount: Infinity,
+	// 	// show cursor
+	// 	showCursor: false
+  // });
